66646166bad678d42ffcbbb428c8dfebde0ebe79
headironc <i@liangzhicheng.cc>
2023-08-21 08:17:20 +0800

    feat: config react-query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "~/components/ui/button";
+
 export default function Page() {
-    return <main></main>;
+    return (
+        <main className="flex min-h-screen items-center justify-center">
+            <Button variant="outline">Click me</Button>
+        </main>
+    );
 }
