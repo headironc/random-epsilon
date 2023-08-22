c57db6ab0f6c845ce935fb0fab18c396c89d40e5
headironc <i@liangzhicheng.cc>
2023-08-22 08:34:14 +0800

    feat: basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,65 @@
-import { Button } from "~/components/ui/button";
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from "~/components/ui/navigation-menu";
+
+const navigations: {
+    text: string;
+    href: string;
+}[] = [
+    {
+        href: "/",
+        text: "首页",
+    },
+    {
+        href: "/east-courtyard/herbal-pharmacy",
+        text: "东院草药房",
+    },
+    {
+        href: "/east-courtyard/pharmacy",
+        text: "东院西药房",
+    },
+    {
+        href: "/west-courtyard/herbal-pharmacy",
+        text: "西院草药房",
+    },
+    {
+        href: "/west-courtyard/pharmacy",
+        text: "西院西药房",
+    },
+];
 
 export default function Page() {
+    const pathname = usePathname();
+
     return (
         <main className="flex min-h-screen items-center justify-center">
-            <Button variant="outline">Click me</Button>
+            <div>
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        {navigations.map(({ href, text }, index) => (
+                            <NavigationMenuItem key={index}>
+                                <Link href={href} legacyBehavior passHref>
+                                    <NavigationMenuLink
+                                        className={navigationMenuTriggerStyle()}
+                                        active={pathname === href}
+                                    >
+                                        {text}
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                        ))}
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
         </main>
     );
 }
