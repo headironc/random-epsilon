2d07713324675f38f93b39c6d30e589864cd72cf
headironc <i@liangzhicheng.cc>
2023-09-18 16:25:09 +0800

    fix

--- src/app/api/patients/herbal-pharmacy/[patient]/route.ts
+++ src/app/api/patients/herbal-pharmacy/[patient]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { setCallSign } from "../route";
+import { CallSign } from "~/types/patient";
 
 export async function PATCH(
     req: NextRequest,
@@ -8,7 +9,7 @@ export async function PATCH(
 ) {
     const FPHM = params.patient;
 
-    setCallSign(FPHM, 0);
+    setCallSign(FPHM, CallSign.NotCalled);
 
     return NextResponse.json("success");
 }
